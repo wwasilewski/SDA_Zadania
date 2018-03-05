899b5d80f53c2059ed1dca2239b449bfc92c9ac3
Wojciech <w.wasilewski@gmail.com>
2018-03-06 00:33:35 +0100

    adding exercise Ogolne_6

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Ogolne_6.java
@@ -0,0 +1,35 @@
+//Wylosuj 1000 liczb z zakresu 1000-2000 i zapisz je do listy. Skorzystaj z stream() w
+//liście do wyszukania najmniejszej i największej wartości.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Ogolne_6 {
+
+    public static void bigAndSmall() {
+        Random random = new Random();
+        ArrayList <Integer> list = new ArrayList<>();
+
+        while (list.size() < 1000) {
+            list.add(random.nextInt(1001) + 1000);
+        }
+        System.out.println("wszystkie wylosowane liczby: \n" + list);
+
+        list.stream()
+                .sorted()
+                .limit(1)
+                .forEach(e -> System.out.println("najmniejsza: " + e));
+
+        list.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(1)
+                .forEach(e -> System.out.print("najwieksza: " + e));
+    }
+
+    public static void main(String[] args) {
+        bigAndSmall();
+    }
+}
