f39c28919979cc3dcaa7cdd98246a4da9cf8b026
Wojciech <w.wasilewski@gmail.com>
2018-03-06 22:40:33 +0100

    adding exercise Stream_4

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Stream_4.java
@@ -0,0 +1,30 @@
+//Utwórz kolekcję z 6 liczbami. Korzystając z strumieni znajdź maksymalną i
+//minimalną liczbę znajdującą się w kolekcji.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Stream_4 {
+    public static void main(String[] args) {
+        int numbers[] = {3, 6, -7, 5, 8, 1};
+        List<Integer> numbersList = new ArrayList<>();
+
+        for (Integer i : numbers) {
+            numbersList.add(i);
+        }
+        System.out.println(numbersList);
+
+        numbersList.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(1)
+                .forEach(n -> System.out.println("biggest: " + n));
+
+        numbersList.stream()
+                .sorted()
+                .limit(1)
+                .forEach(n -> System.out.println("smallest: " + n));
+    }
+}
