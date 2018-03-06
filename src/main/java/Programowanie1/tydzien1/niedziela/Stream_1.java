ede1dd1d35720d82a0c1fd28ca5fe7f244269a89
Wojciech <w.wasilewski@gmail.com>
2018-03-06 20:33:30 +0100

    adding exercise Stream_1

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Stream_1.java
@@ -0,0 +1,24 @@
+//Utwórz kolekcję, na następnie dodaj do niej 5 imion. Korzystając z Stream zmodyfikuj
+//kolekcję tak, aby imiona były pisane dużymi literami (UPPERCASE), a następnie
+//wyświetl wszystkie imiona.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Stream_1 {
+    public static void main(String[] args) {
+        String names[] = {"Ania", "Kasia", "Basia", "Magda", "Wojtek"};
+
+        List<String> namesList = new ArrayList<>(Arrays.asList(names));
+        //mozna tak jak wyzej lub -> namesList.addAll(Arrays.asList(names));
+
+        System.out.println(namesList);
+
+        namesList.stream()
+                .map(i -> i.toUpperCase())
+                .forEach(i -> System.out.print(i + " "));
+    }
+}
