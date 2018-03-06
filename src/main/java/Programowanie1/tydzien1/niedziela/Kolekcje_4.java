2fd0c6b1c64121f950dd22c833591f39ac735e40
Wojciech <w.wasilewski@gmail.com>
2018-03-06 19:03:04 +0100

    adding exercise Kolekcje_4

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Kolekcje_4.java
@@ -0,0 +1,23 @@
+//Jesteś firmą produkującą tabliczki z imionami. Dla każdego imienia musisz
+//przygotować matrycę. Raz przygotowana matryca może być wykorzystywana
+//wielokrotnie. Korzystając z odpowiedniej kolekcji dodaj do niej imiona (co najmniej
+//10) osób tak aby w kolekcji się nie powtarzały. Podczas dodawania dodaj kilka imion
+//powtarzających się.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Kolekcje_4 {
+    public static void main(String[] args) {
+
+        String names[] = {"Anna", "Kasia", "Basia", "Anna", "Frania", "Tomek", "Marek", "Basia", "Bamberiusz", "Marek"};
+        Set<String> uniqueNames = new HashSet<>();
+        uniqueNames.addAll(Arrays.asList(names));
+
+        System.out.println("Original names: " + Arrays.toString(names));
+        System.out.println("HashSet names: " + uniqueNames);
+    }
+}
