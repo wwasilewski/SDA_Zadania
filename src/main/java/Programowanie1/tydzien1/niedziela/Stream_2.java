683d2558046e4b879dd81c6abf2ed7bcf91bf76a
Wojciech <w.wasilewski@gmail.com>
2018-03-06 20:48:28 +0100

    adding exercise Stream_2

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Stream_2.java
@@ -0,0 +1,20 @@
+//Utwórz kolekcję z 10 imionami (W kolekcji musi pojawić się imię Jan). Korzystając
+//ze strumieni znajdź imię Jan i wyświetl je.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Stream_2 {
+    public static void main(String[] args) {
+        String names[] = {"Anna", "Kasia", "Basia", "Jan", "Jania", "Tomek", "Jan", "Janek", "Bamberiusz", "Janislaw"};
+        List<String> namesList = new ArrayList<>(Arrays.asList(names));
+        System.out.println(namesList);
+
+        namesList.stream()
+                .filter(s -> s.equals("Jan"))
+                .forEach(s -> System.out.println(s));
+    }
+}
