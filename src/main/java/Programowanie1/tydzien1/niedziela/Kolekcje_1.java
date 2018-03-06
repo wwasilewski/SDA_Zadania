f274b1efd6d8159d076661075b704b7a1ffaa1cd
Wojciech <w.wasilewski@gmail.com>
2018-03-06 01:24:18 +0100

    adding exercise Kolekcje_1

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Kolekcje_1.java
@@ -0,0 +1,25 @@
+//Dodaj 5 imion do kolekcji. Wybierz tą kolekcję, która posortuje wprowadzone
+//wartości. Po dodaniu elementów wyświetl wszystkie.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kolekcje_1 {
+    public static void main(String[] args) {
+        String names[] = {"Barbara", "Maria", "Kaska", "Dorota", "Anna"};
+        List<String> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(names));
+
+//        for (String s : names) {
+//            list.add(s);
+//        }
+
+        list.stream()
+                .sorted()
+                .forEach(e -> System.out.print(e + " "));
+    }
+}
