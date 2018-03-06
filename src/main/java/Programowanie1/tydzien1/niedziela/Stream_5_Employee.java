54772e6211a66a509135dc7ce7050028c67b6c86
Wojciech <w.wasilewski@gmail.com>
2018-03-06 23:51:54 +0100

    adding exercise Stream_5

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Stream_5_Employee.java
@@ -0,0 +1,20 @@
+//Utwórz klasę pracownik posiadającą imię, nazwisko i wynagrodzenie. Dodaj 10
+//pracowników do kolekcji ArrayList. Korzystając ze strumieni znajdź średnie
+//wynagrodzenie pracownika, a następnie korzystając ze strumieni wyświetl wszystkich
+//pracowników zarabiających więcej niż średnia.
+
+package Programowanie1.tydzien1.niedziela;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Stream_5_Employee {
+    String name;
+    String surname;
+    int salary;
+}
---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Stream_5_EmployeeRun.java
@@ -0,0 +1,37 @@
+//Utwórz klasę pracownik posiadającą imię, nazwisko i wynagrodzenie. Dodaj 10
+//pracowników do kolekcji ArrayList. Korzystając ze strumieni znajdź średnie
+//wynagrodzenie pracownika, a następnie korzystając ze strumieni wyświetl wszystkich
+//pracowników zarabiających więcej niż średnia.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream_5_EmployeeRun {
+    public static void main(String[] args) {
+        List<Stream_5_Employee> employeeList = new ArrayList<>();
+
+        employeeList.add(new Stream_5_Employee("Marek", "M", 6400));
+        employeeList.add(new Stream_5_Employee("Ania", "A", 3400));
+        employeeList.add(new Stream_5_Employee("Wojtek", "W", 20000));
+        employeeList.add(new Stream_5_Employee("Tomek", "T", 5000));
+        employeeList.add(new Stream_5_Employee("Magda", "M", 4000));
+        employeeList.add(new Stream_5_Employee("Jan", "J", 4500));
+        employeeList.add(new Stream_5_Employee("Franek", "F", 7600));
+        employeeList.add(new Stream_5_Employee("Arek", "A", 2200));
+        employeeList.add(new Stream_5_Employee("Karolina", "K", 9060));
+        employeeList.add(new Stream_5_Employee("Zenon", "Z", 2000));
+
+        Double avgSalary = employeeList.stream()
+                .collect(Collectors.averagingInt(s -> s.salary));
+        System.out.println("Average salary: " + avgSalary);
+
+        List<Stream_5_Employee> richList = employeeList.stream()
+                .filter(s -> s.salary > avgSalary)
+                .collect(Collectors.toList());
+        System.out.print("> average salary: ");
+        richList.forEach(s -> System.out.print(s.name + " " + s.surname + "(" + s.salary + ") "));
+    }
+}
