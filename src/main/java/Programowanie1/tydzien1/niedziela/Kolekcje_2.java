b8bd49ce86f3d2b9bfec303dc9e7d9fcaaa875d5
Wojciech <w.wasilewski@gmail.com>
2018-03-06 01:44:01 +0100

    adding exercise Kolekcje_2

---
+++ src/main/java/Programowanie1/tydzien1/niedziela/Kolekcje_2.java
@@ -0,0 +1,20 @@
+//Dodaj 10 losowych liczb do kolekcji. Korzystając z indeksów pobierz po kolei
+//wszystkie elementy i wyświetl je.
+
+package Programowanie1.tydzien1.niedziela;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Kolekcje_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(100));
+            System.out.println("liczba nr" + (i + 1) + " to: " + list.get(i));
+        }
+    }
+}
