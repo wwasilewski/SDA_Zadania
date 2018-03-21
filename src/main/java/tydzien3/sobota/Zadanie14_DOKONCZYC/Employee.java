a6970093d23ae103733e1bfdcfb74e5d3d4dcda2
Wojciech <w.wasilewski@gmail.com>
2018-03-21 22:16:03 +0100

    adding zalegle ex14 Zadania02 typy generyczne - NIESKONCZONE

---
+++ src/main/java/tydzien3/sobota/Zadanie14_DOKONCZYC/Employee.java
@@ -0,0 +1,11 @@
+//Przygotuj klasę BasePerson posiadającą pola name oraz surname, klasę Employee
+//dziedziczącą z klasy BasePerson posiadającą pole companyName, klasę Teacher
+//dziedziczącą z klasy BasePerson posiadającą pole school, degree. Wykorzystaj
+//klasę Clipboard z poprzedniego zadania, ale zmodyfikuj ją tak, aby przyjmowała
+//klasę BasePerson oraz jej wszystkie klasy potomne.
+
+package tydzien3.sobota.Zadanie14_DOKONCZYC;
+
+public class Employee extends BasePerson {
+    private String companyName;
+}
